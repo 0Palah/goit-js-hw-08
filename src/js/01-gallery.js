aa67e8f1daa4548a44b92f320502c31f489284c8
Oleg Palashynskyi <palah@ukr.net>
2022-09-06 23:08:37 +0300

    add vimeo

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryListEl = document.querySelector('.gallery');
 
+const makeGalleryCard = ({ preview, original, description } = {}) => {
+  const galleryCard = `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  return galleryCard;
+};
+
+const makeGalleryArr = galleryItems.map(el => makeGalleryCard(el));
+// console.log(makeGalleryArr);
+// console.log(galleryListEl);
+galleryListEl.insertAdjacentHTML('beforeend', makeGalleryArr.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const iframeEl = document.querySelector('#vimeo-player');
+console.log(iframeEl);
+
+const player = new Player(iframeEl);
+console.log(player);
+
+const currentVideoTime = {};
+player.on('timeupdate', function (data) {
+  console.log(data);
+  let time = `"videoplayer-current-time": ${data.seconds}`;
+  console.log(time);
+});
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
