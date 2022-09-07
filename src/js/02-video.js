d019be8e231a634d8bf3b955db2cce50ec64cc65
Oleg Palashynskyi <palah@ukr.net>
2022-09-07 15:58:12 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframeEl = document.querySelector('#vimeo-player');
 console.log(iframeEl);
@@ -6,13 +7,12 @@ console.log(iframeEl);
 const player = new Player(iframeEl);
 console.log(player);
 
-const currentVideoTime = {};
-player.on('timeupdate', function (data) {
+const currentVideoTime = data => {
   console.log(data);
-  let time = `"videoplayer-current-time": ${data.seconds}`;
-  console.log(time);
-});
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
 
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+player.on('timeupdate', throttle(currentVideoTime, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+console.log(localStorage.getItem('videoplayer-current-time'));
